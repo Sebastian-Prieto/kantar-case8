89bc360bb3cb4ca6967b11977a51dc8b620a551d
Sebastian <sbprietoc@gmail.com>
2023-06-29 14:54:49 -0500

    First commit

---
+++ index.mjs
@@ -0,0 +1,14 @@
+import lodash from "lodash"
+
+// function removeVowels(text) {
+    // const vowels = ['a', 'e', 'i', 'o', 'u'];
+// }
+
+let text = "Manzana";
+let result = text.replace(/[aeiou]/gi, '');
+
+console.log(result);
+
+// function removeVowelsKantar(texto) {
+//     return texto.replace(/[aeiou]/gi, '');
+// }
